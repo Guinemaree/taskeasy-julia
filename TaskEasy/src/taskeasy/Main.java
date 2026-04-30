c3f7d565f75a0a19cb62c476bf038a0f8610c059
Júlia Prado Matos <julia.matos2@estudante.ifms.edu.br>
2026-04-30 08:58:15 -0400

    Primeiro commit do projeto

---
+++ TaskEasy/src/taskeasy/Main.java
@@ -0,0 +1,16 @@
+package taskeasy;
+
+/**
+ *
+ * @author Júlia Prado Matos 1212B
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            System.out.println("Bem-vindo ao sistema TaskEasy");
+    }
+    
+}
